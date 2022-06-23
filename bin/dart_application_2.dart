d01d884f4aaf67615b00893a0df4715010e3cce4
isachef <isa.smail1123@gmail.com>
2022-06-23 17:55:21 +0600

    first commit

---
+++ bin/dart_application_2.dart
@@ -0,0 +1,27 @@
+void main() {
+//1
+  var a = 0;
+  if (a == 10)
+    print('correct');
+  else
+    print('incorrect');
+  if (a == 0)
+    print('correct');
+  else
+    print('incorrect');
+  if (a > 0)
+    print('correct');
+  else
+    print('incorrect');
+  if (a <= 0)
+    print('correct');
+  else
+    print('incorrect');
+  if (a == 'test')
+    print('correct');
+  else
+    print('incorrect');
+    if(a=='1' && a.runtimeType==String) print('correct');
+  else
+    print('incorrect');
+}
